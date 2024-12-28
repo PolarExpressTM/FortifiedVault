2817d28e1f59df2dde853e8430f1f86bb427919f
Schnitzel5 <tangphilip3@gmail.com>
2024-12-28 01:33:32 +0100

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'fortified-db',
       port: 3306,
       username: 'root',
       password: 'root',
